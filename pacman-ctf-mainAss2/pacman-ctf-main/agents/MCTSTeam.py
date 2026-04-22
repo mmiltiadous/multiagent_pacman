d5710017dfd7e38a226aac12fdf456c96e2efe36
Myriana Miltiadous <125773136+mmiltiadous@users.noreply.github.com>
2026-04-23 01:22:06 +0200

    Delete pacman-ctf-mainAss2/pacman-ctf-main directory

--- pacman-ctf-mainAss2/pacman-ctf-main/agents/MCTSTeam.py
+++
@@ -1,8 +0,0 @@
-
-from agents.MCTSAgents import OffensiveAgent, DefensiveAgent
-
-def createTeam(
-    firstIndex, secondIndex, isRed, first="OffensiveAgent", second="DefensiveAgent"
-):
-    return [eval(first)(firstIndex), eval(second)(secondIndex)]
-
